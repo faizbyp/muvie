100804c8a76ef4737bd0d0b97d5f7271865c151c
faizbyp <faizbyp@gmail.com>
2024-11-07 23:09:43 +0700

    feat: sort and filter

--- src/app/app/App.tsx
+++ src/app/app/App.tsx
@@ -2,10 +2,23 @@
 import MovieCard from "@/components/MovieCard";
 import { CardSkeleton } from "@/components/Skeleton";
 import useFetch from "@/hooks/useFetch";
+import { NumericFormat } from "react-number-format";
 import useUserStore from "@/hooks/useUserStore";
 import { Movie } from "@/types/Movie";
 import { fetcher } from "@/utils/api";
-import { Box, Container, Typography, Grid2 as Grid, Pagination } from "@mui/material";
+import {
+  Box,
+  Container,
+  Typography,
+  Grid2 as Grid,
+  Pagination,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  SelectChangeEvent,
+  TextField,
+} from "@mui/material";
 import { ChangeEvent, useState } from "react";
 import useSWR from "swr";
 
@@ -13,15 +26,71 @@ interface AppProps {
   movies: Movie[];
 }
 
+const sortBy = [
+  {
+    name: "---",
+    value: "",
+  },
+  {
+    name: "Original Title - Ascending",
+    value: "original_title.asc",
+  },
+  {
+    name: "Original Title - Descending",
+    value: "original_title.desc",
+  },
+  {
+    name: "Popularity - Ascending",
+    value: "popularity.asc",
+  },
+  {
+    name: "Popularity - Descending",
+    value: "popularity.desc",
+  },
+  {
+    name: "Release Date - Ascending",
+    value: "primary_release_date.asc",
+  },
+  {
+    name: "Release Date - Descending",
+    value: "primary_release_date.desc",
+  },
+  {
+    name: "Vote Average - Ascending",
+    value: "vote_average.asc",
+  },
+  {
+    name: "Vote Average - Descending",
+    value: "vote_average.desc",
+  },
+];
+
 const App = () => {
   const name = useUserStore((state) => state.name);
   const bio = useUserStore((state) => state.bio);
+  const [sort, setSort] = useState("");
+  const [year, setYear] = useState<string | number | undefined>();
   const [page, setPage] = useState(1);
+
   // use swr to cache data
-  const { data: movies } = useSWR(`/discover/movie?page=${page}`, fetcher, {
+  const url = `/discover/movie?page=${page}&sort_by=${sort}&primary_release_year=${year}`;
+  const { data: movies, error } = useSWR(url, fetcher, {
     keepPreviousData: true, // cache data
   });
   // const { data: movies } = useFetch<any>(`/discover/movie?page=${page}`);
+
+  const handleSort = (event: SelectChangeEvent) => {
+    setSort(event.target.value as string);
+  };
+
+  const handleYear = async (event: ChangeEvent<HTMLInputElement>) => {
+    await new Promise<void>((resolve) => {
+      setTimeout(() => {
+        setYear(event.target.value);
+        resolve();
+      }, 500);
+    });
+  };
 
   const handlePage = (event: ChangeEvent<unknown>, value: number) => {
     setPage(value);
@@ -65,18 +134,41 @@ const App = () => {
         <Typography variant="h1" color="primary">
           Discover Muvies
         </Typography>
-        <Box sx={{ display: "flex", justifyContent: "center", my: 4 }}>
-          <Pagination
-            count={10}
-            page={page}
-            onChange={handlePage}
-            shape="rounded"
-            color="primary"
-          />
-        </Box>
         <Grid container spacing={2}>
-          {movies ? (
-            movies.results.map((movie: Movie) => (
+          <Grid size={{ xs: 12, md: 6 }}>
+            <FormControl fullWidth>
+              <InputLabel>Sort By</InputLabel>
+              <Select label="Sort By" value={sort} fullWidth onChange={handleSort}>
+                {sortBy.map((sort, index) => (
+                  <MenuItem key={index} value={sort.value}>
+                    {sort.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid size={{ xs: 12, md: 6 }}>
+            <FormControl fullWidth sx={{ mb: 2 }}>
+              <NumericFormat
+                value={year}
+                label="Release Year"
+                customInput={TextField}
+                fullWidth
+                allowNegative={false}
+                onChange={handleYear}
+                slotProps={{
+                  htmlInput: {
+                    maxLength: 4,
+                  },
+                }}
+              />
+            </FormControl>
+          </Grid>
+        </Grid>
+        {error && error.info}
+        {movies ? (
+          <Grid container spacing={2}>
+            {movies.results.map((movie: Movie) => (
               <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }} key={movie.id}>
                 <MovieCard
                   title={movie.title}
@@ -84,12 +176,12 @@ const App = () => {
                   vote_average={movie.vote_average}
                 />
               </Grid>
-            ))
-          ) : (
-            <CardSkeleton />
-          )}
-        </Grid>
-        <Box sx={{ display: "flex", justifyContent: "center", my: 4 }}>
+            ))}
+          </Grid>
+        ) : (
+          <CardSkeleton />
+        )}
+        <Box sx={{ display: "flex", justifyContent: "center", my: 2 }}>
           <Pagination
             count={10}
             page={page}
